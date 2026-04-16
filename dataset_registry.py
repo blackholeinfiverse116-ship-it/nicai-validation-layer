1326b750f2a2ee88d1ca83ce3c7ec771d6b0b8e7
blackholeinfiverse116-ship-it <blackholeinfiverse116@gmail.com>
2026-04-16 17:44:11 +0530

    Update dataset_registry.py

--- dataset_registry.py
+++ dataset_registry.py
@@ -4,7 +4,7 @@ import json
 def load_datasets():
     try:
         with open("datasets.json", "r") as f:
-            return json.load(f) ा
+            return json.load(f)
     except FileNotFoundError:
         print("ERROR: datasets.json file not found")
         return {}
